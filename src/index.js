f4b70f5644a9bb875e806fd836966d879cb6d220
PlSairam <plsairam13@gmail.com>
2023-10-12 12:19:58 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import HeaderBar from './HeaderBar';
 import reportWebVitals from './reportWebVitals';
+import Sectionbar from './Sectionbar';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,6 +11,7 @@ root.render(
   <React.StrictMode>
      <BrowserRouter>
        <HeaderBar/>
+       <Sectionbar/>
      </BrowserRouter>
   </React.StrictMode>
 );
