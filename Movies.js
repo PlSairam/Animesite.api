f5ed225f8cb2ba7abfb6f76b92361c9d0b49c97d
PlSairam <147598119+PlSairam@users.noreply.github.com>
2023-10-16 10:25:42 +0530

    Add files via upload

---
+++ Movies.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import "./Header.css";
+function movies() {
+  return (
+    <div>
+    <div class="container">
+             <div className="abc">
+               <img width="150px" src="https://img.flawlessfiles.com/_r/300x400/100/16/ee/16eec56baf8f3fcc6430607f58ce3d12/16eec56baf8f3fcc6430607f58ce3d12.jpg"/> 
+               <a href="https://aniwatch.to/your-name-10?ref=search" style={{color:"Black"}}><b>Your Name</b></a>
+              </div>
+              <div className="abc">
+               <img width="150px" src="https://img.flawlessfiles.com/_r/300x400/100/7f/c6/7fc61d82b89949dff4319bd73ff2d456/7fc61d82b89949dff4319bd73ff2d456.jpg"/> 
+               <a href="https://aniwatch.to/a-silent-voice-11?ref=search" style={{color:"Black"}}><b>A Silent Voice</b></a>
+              </div><div className="abc">
+               <img width="150px" src="https://img.flawlessfiles.com/_r/300x400/100/e2/c6/e2c621df60f4deb6693b8db0d391b85c/e2c621df60f4deb6693b8db0d391b85c.jpg"/> 
+               <a href="https://aniwatch.to/5-centimeters-per-second-859?ref=search" style={{color:"Black"}}><b>5 Centimeters Per Second</b></a>
+              </div>
+              <div className="abc">
+               <img width="150px" src="https://img.flawlessfiles.com/_r/300x400/100/1d/22/1d226c74f7248dda6aac32d8f915b8f1/1d226c74f7248dda6aac32d8f915b8f1.jpg"/> 
+               <a href="https://aniwatch.to/spirited-away-20?ref=search" style={{color:"Black"}}><b>Spirited Away</b></a>
+              </div>
+              <div className="abc">
+               <img width="150px" src="https://img.flawlessfiles.com/_r/300x400/100/fb/2c/fb2c118f256e459bfc3d283f170f7b47/fb2c118f256e459bfc3d283f170f7b47.jpg"/> 
+               <a href="https://aniwatch.to/ponyo-671?ref=search" style={{color:"Black"}}><b>Ponyo</b></a>
+              </div>
+              <div className="abc">
+               <img width="150px" src="https://img.flawlessfiles.com/_r/300x400/100/cc/22/cc22d05c49a0c14672e8d7a05f1ddcc8/cc22d05c49a0c14672e8d7a05f1ddcc8.jpg"/> 
+               <a href="https://aniwatch.to/bubble-18009?ref=search" style={{color:"Black"}}><b>Bubble</b></a>
+              </div>
+              <div className="abc">
+               <img width="150px" src="https://img.flawlessfiles.com/_r/300x400/100/30/d7/30d734a5c62ba302c1bf70733408f5e2/30d734a5c62ba302c1bf70733408f5e2.jpg"/> 
+               <a href="https://aniwatch.to/a-whisker-away-1772?ref=search" style={{color:"Black"}}><b>A Whisker Away</b></a>
+              </div>
+              <div className="abc">
+               <img width="150px" src="https://img.flawlessfiles.com/_r/300x400/100/37/eb/37eb9509a2ae0010f64b0170a03b5a13/37eb9509a2ae0010f64b0170a03b5a13.jpg"/> 
+               <a href="https://aniwatch.to/weathering-with-you-111?ref=search" style={{color:"Black"}}><b>Weathering With You</b></a>
+              </div>
+              <div className="abc">
+               <img width="150px" src="https://img.flawlessfiles.com/_r/300x400/100/9b/55/9b5588c2990b97578ddb3fb0db48baa7/9b5588c2990b97578ddb3fb0db48baa7.jpg"/> 
+               <a href="https://aniwatch.to/batman-ninja-7371" style={{color:"Black"}}><b>Batman Ninja</b></a>
+              </div>
+              <div className="abc">
+               <img width="150px" src="https://img.flawlessfiles.com/_r/300x400/100/ca/6f/ca6faf0784e4233fc3fdfbae441eb789/ca6faf0784e4233fc3fdfbae441eb789.jpg"/> 
+               <a href="https://aniwatch.to/rezero-kara-hajimeru-isekai-seikatsu-memory-snow-1473" style={{color:"Black"}}><b>Re:Zero</b></a>
+              </div>
+              <div className="abc">
+               <img width="150px" src="https://img.flawlessfiles.com/_r/300x400/100/6a/6d/6a6d4e47bca47ad4eb3656dbc606ea23/6a6d4e47bca47ad4eb3656dbc606ea23.jpg"/> 
+               <a href="https://aniwatch.to/i-want-to-eat-your-pancreas-70?ref=search" style={{color:"Black"}}><b>I want to eat your pancreas</b></a>
+              </div>
+              <div className="abc">
+               <img width="150px" src="https://img.flawlessfiles.com/_r/300x400/100/88/d7/88d7186ebed8856c731b2dd132aaf68a/88d7186ebed8856c731b2dd132aaf68a.jpg"/> 
+               <a href="https://aniwatch.to/suzumes-doorlocking-18190?ref=search" style={{color:"Black"}}><b>Suzume’s Door-Locking</b></a>
+              </div>
+              <div className="abc">
+               <img width="150px" src="https://img.flawlessfiles.com/_r/300x400/100/fd/24/fd2425b4fb8fe7aec860e4706d981392/fd2425b4fb8fe7aec860e4706d981392.jpg"/> 
+               <a href="https://aniwatch.to/the-garden-of-words-414" style={{color:"Black"}}><b>The Garden of Words</b></a>
+              </div>
+              <div className="abc">
+               <img width="150px" src=""/> 
+               <a href="" style={{color:"Black"}}><b></b></a>
+              </div>
+              <div className="abc">
+               <img width="150px" src=""/> 
+               <a href="" style={{color:"Black"}}><b></b></a>
+              </div>
+              <div className="abc">
+               <img width="150px" src=""/> 
+               <a href="" style={{color:"Black"}}><b></b></a>
+              </div>
+
+         </div>
+    </div>
+  )
+}
+export default movies
